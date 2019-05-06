97ab4cd9fd019dc682ff4cdea73817d361323839
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-05-06 08:27:35 -0500

    TEST_CASE macro

--- src/cpu.asm
+++ src/cpu.asm
@@ -15,6 +15,16 @@ RESET: MACRO
        pop de
        ENDM
 
+TEST_CASE: MACRO
+           RESET 0
+           \1
+           DUMP
+
+           RESET $ffff
+           \1
+           DUMP
+           ENDM
+
 SECTION "header", HOME[$100]
     nop
     jp $150
@@ -28,86 +38,50 @@ SECTION "main", HOME[$150]
     di
     ld sp, $e000
 
-_x03: ; INC BC
-    RESET 0
-    inc bc
-    DUMP ; DFF8 <- 00 00 01 00 00 00 00 00
+    ; OP 03
+    TEST_CASE inc bc
+    ; DFF8 <- 00 00 01 00 00 00 00 00
+    ; DFF0 <- F0 FF FF 00 01 00 FF 00
 
-    RESET $ffff
-    inc bc
-    DUMP ; DFF8 <- F0 FF FF 00 01 00 FF 00
+    ; OP 04
+    TEST_CASE inc b
+    ; DFE8 <- 00 00 00 01 00 00 00 00
+    ; DFE0 <- B0 FF FF 00 FF FF FF FF
 
-_x04: ; INC B
-    RESET 0
-    inc b
-    DUMP ; DFE8 <- 00 00 00 01 00 00 00 00
+    ; OP 05
+    TEST_CASE dec b
+    ; DFD8 <- 60 00 00 FF 00 00 00 00
+    ; DFD0 <- 50 FF FF FE FF FF FF FF
 
-    RESET $ffff
-    inc b
-    DUMP ; DFE0 <- B0 FF FF 00 FF FF FF FF
+    ; OP 07
+    TEST_CASE rlca
+    ; DFC8 <- 60 00 00 FF 00 00 00 00
+    ; DFC0 <- 50 FF FF FE FF FF FF FF
 
-_x05: ; DEC B
-    RESET 0
-    dec b
-    DUMP ; DFD8 <- 60 00 00 FF 00 00 00 00
+    ; OP 09
+    TEST_CASE add hl,bc
+    ; DFB8 <- 60 00 00 FF 00 00 00 00
+    ; DFB0 <- 50 FF FF FE FF FF FF FF
 
-    RESET $ffff
-    dec b
-    DUMP ; DFD0 <- 50 FF FF FE FF FF FF FF
+    ; OP 0B
+    TEST_CASE dec bc
+    ; DFA8 <- 60 00 00 FF 00 00 00 00
+    ; DFA0 <- 50 FF FF FE FF FF FF FF
 
-_x07: ; RLCA
-    RESET 0
-    rlca
-    DUMP ; DFC8 <- 60 00 00 FF 00 00 00 00
+    ; OP 0C
+    TEST_CASE inc c
+    ; DF98 <- 60 00 00 FF 00 00 00 00
+    ; DF90 <- 50 FF FF FE FF FF FF FF
 
-    RESET $ffff
-    rlca
-    DUMP ; DFC0 <- 50 FF FF FE FF FF FF FF
+    ; OP 0D
+    TEST_CASE dec c
+    ; DF88 <- 60 00 00 FF 00 00 00 00
+    ; DF80 <- 50 FF FF FE FF FF FF FF
 
-_x09: ; ADD HL,BC
-    RESET 0
-    add hl,bc
-    DUMP ; DFB8 <- 60 00 00 FF 00 00 00 00
-
-    RESET $ffff
-    add hl,bc
-    DUMP ; DFB0 <- 50 FF FF FE FF FF FF FF
-
-_x0B: ; DEC BC
-    RESET 0
-    dec bc
-    DUMP ; DFA8 <- 60 00 00 FF 00 00 00 00
-
-    RESET $ffff
-    dec bc
-    DUMP ; DFA0 <- 50 FF FF FE FF FF FF FF
-
-_x0C: ; INC C
-    RESET 0
-    inc c
-    DUMP ; DF98 <- 60 00 00 FF 00 00 00 00
-
-    RESET $ffff
-    inc c
-    DUMP ; DF90 <- 50 FF FF FE FF FF FF FF
-
-_x0D: ; DEC C
-    RESET 0
-    dec c
-    DUMP ; DF88 <- 60 00 00 FF 00 00 00 00
-
-    RESET $ffff
-    dec b
-    DUMP ; DF80 <- 50 FF FF FE FF FF FF FF
-
-_x0F: ; RRCA
-    RESET 0
-    rrca
-    DUMP ; DF88 <- 60 00 00 FF 00 00 00 00
-
-    RESET $ffff
-    rrca
-    DUMP ; DF80 <- 50 FF FF FE FF FF FF FF
+    ; OP 0F
+    TEST_CASE rrca
+    ; DF88 <- 60 00 00 FF 00 00 00 00
+    ; DF80 <- 50 FF FF FE FF FF FF FF
 
 endloop:
     jp endloop
