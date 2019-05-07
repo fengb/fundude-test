a57eb02a483be0fc9078600ac35de2196f8e6fdf
Benjamin Feng <benjamin.feng@glassdoor.com>
2019-05-07 00:50:29 -0500

    Toss in OP 8x

--- src/cpu.asm
+++ src/cpu.asm
@@ -168,10 +168,241 @@ AxDEE0: MACRO
         DB $00,$00,$00,$00,$00,$00,$00,$00
         ENDM
 
+; ----
+
+    ; OP 23
+    TEST_CASE inc hl
+AxDED0: MACRO
+        DB $F0,$FF,$FF,$FF,$FF,$FF,$00,$00
+        DB $00,$00,$00,$00,$00,$00,$01,$00
+        ENDM
+
+    ; OP 24
+    TEST_CASE inc h
+AxDEC0: MACRO
+        DB $B0,$FF,$FF,$FF,$FF,$FF,$FF,$00
+        DB $00,$00,$00,$00,$00,$00,$00,$01
+        ENDM
+
+    ; OP 25
+    TEST_CASE dec h
+AxDEB0: MACRO
+        DB $50,$FF,$FF,$FF,$FF,$FF,$FF,$FE
+        DB $60,$00,$00,$00,$00,$00,$00,$FF
+        ENDM
+
+    ; OP 27
+    TEST_CASE daa
+AxDEA0: MACRO
+        DB $50,$99,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 29
+    TEST_CASE add hl,hl
+AxDE90: MACRO
+        DB $B0,$FF,$FF,$FF,$FF,$FF,$FE,$FF
+        DB $00,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 2B
+    TEST_CASE dec hl
+AxDE80: MACRO
+        DB $F0,$FF,$FF,$FF,$FF,$FF,$FE,$FF
+        DB $00,$00,$00,$00,$00,$00,$FF,$FF
+        ENDM
+
+    ; OP 2C
+    TEST_CASE inc l
+AxDE70: MACRO
+        DB $B0,$FF,$FF,$FF,$FF,$FF,$00,$FF
+        DB $00,$00,$00,$00,$00,$00,$01,$00
+        ENDM
+
+    ; OP 2D
+    TEST_CASE dec l
+AxDE60: MACRO
+        DB $50,$FF,$FF,$FF,$FF,$FF,$FE,$FF
+        DB $60,$00,$00,$00,$00,$00,$FF,$00
+        ENDM
+
+    ; OP 2F
+    TEST_CASE cpl
+AxDE50: MACRO
+        DB $F0,$00,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $60,$FF,$00,$00,$00,$00,$00,$00
+        ENDM
+
+; ----
+
+    ; OP 37
+    TEST_CASE scf
+AxDE40: MACRO
+        DB $90,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $10,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 39
+    ; TODO -- this is buggy and outputs 0x29 (add hl,hl)
+    TEST_CASE add hl,sp
+AxDE30: MACRO
+        DB $B0,$FF,$FF,$FF,$FF,$FF,$FE,$FF
+        DB $00,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 3C
+    TEST_CASE inc a
+AxDE20: MACRO
+        DB $B0,$00,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $00,$01,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 3D
+    TEST_CASE dec a
+AxDE10: MACRO
+        DB $50,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $60,$FF,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 3F
+    TEST_CASE ccf
+AxDE00: MACRO
+        DB $80,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $10,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+; ----
+
+    ; OP 80
+    TEST_CASE add a,b
+AxDDF0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 81
+    TEST_CASE add a,c
+AxDDE0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 82
+    TEST_CASE add a,d
+AxDDD0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 83
+    TEST_CASE add a,e
+AxDDC0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 84
+    TEST_CASE add a,h
+AxDDB0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 85
+    TEST_CASE add a,l
+AxDDA0: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 87
+    TEST_CASE add a,a
+AxDD90: MACRO
+        DB $30,$FE,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 88
+    TEST_CASE adc a,b
+AxDD80: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 89
+    TEST_CASE adc a,c
+AxDD70: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 8A
+    TEST_CASE adc a,d
+AxDD60: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 8B
+    TEST_CASE adc a,e
+AxDD50: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 8C
+    TEST_CASE adc a,h
+AxDD40: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 8D
+    TEST_CASE adc a,l
+AxDD30: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
+    ; OP 8F
+    TEST_CASE adc a,a
+AxDD20: MACRO
+        DB $30,$FF,$FF,$FF,$FF,$FF,$FF,$FF
+        DB $80,$00,$00,$00,$00,$00,$00,$00
+        ENDM
+
 endloop:
     jp endloop
 
 SECTION "data", ROMX[$4000]
+    AxDD20
+    AxDD30
+    AxDD40
+    AxDD50
+    AxDD60
+    AxDD70
+    AxDD80
+    AxDD90
+    AxDDA0
+    AxDDB0
+    AxDDC0
+    AxDDD0
+    AxDDE0
+    AxDDF0
+    AxDE00
+    AxDE10
+    AxDE20
+    AxDE30
+    AxDE40
+    AxDE50
+    AxDE60
+    AxDE70
+    AxDE80
+    AxDE90
+    AxDEA0
+    AxDEB0
+    AxDEC0
+    AxDED0
     AxDEE0
     AxDEF0
     AxDF00
